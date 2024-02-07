7dd613a19125c382c10760cf70a2c5552840b8ea
LucaGoubelle <luca.goubelle97@gmail.com>
2024-02-07 21:03:07 +0100

    Test face moves

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 
 from megaminx import Megaminx
 from megaminx_drawer import MegaminxDrawer
+from moves.mover import Mover
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -13,13 +14,26 @@ bgcolor = (32,32,32)
 # -----------------------
 minx = Megaminx(full=True)
 minx_drawer = MegaminxDrawer()
+minx_mover = Mover()
 # -----------------------
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_u:
+                minx = minx_mover.simpleMove(minx, "U'") if event.mod & pygame.KMOD_SHIFT else minx_mover.simpleMove(minx, "U")
+            if event.key == pygame.K_l:
+                minx = minx_mover.simpleMove(minx, "L'") if event.mod & pygame.KMOD_SHIFT else minx_mover.simpleMove(minx, "L")
+            if event.key == pygame.K_r:
+                minx = minx_mover.simpleMove(minx, "R'") if event.mod & pygame.KMOD_SHIFT else minx_mover.simpleMove(minx, "R")
+            if event.key == pygame.K_f:
+                minx = minx_mover.simpleMove(minx, "F'") if event.mod & pygame.KMOD_SHIFT else minx_mover.simpleMove(minx, "F")
+            if event.key == pygame.K_d:
+                minx = minx_mover.simpleMove(minx, "DL'") if event.mod & pygame.KMOD_SHIFT else minx_mover.simpleMove(minx, "DL")
+        
         screen.fill(bgcolor)
         #------------------------------
         minx_drawer.draw(screen, minx)
--- moves/mover.py
+++ moves/mover.py
@@ -15,6 +15,40 @@ class Mover:
     def sleeper(t):
         # t = millisecond
         pygame.time.wait(t)
+        
+    def simpleMove(self, minx, move):
+        match move:
+            case "U": minx = move_U(minx)
+            case "U'": minx = move_U_prime(minx)
+            case "U2": minx = move_U2(minx)
+            case "L": minx = move_L(minx)
+            case "L'": minx = move_L_prime(minx)
+            case "L2": minx = move_L2(minx)
+            case "R": minx = move_R(minx)
+            case "R'": minx = move_R_prime(minx)
+            case "R2": minx = move_R2(minx)
+            case "F": minx = move_F(minx)
+            case "F'": minx = move_F_prime(minx)
+            case "F2": minx = move_F2(minx)
+            case "DL": minx = move_DL(minx)
+            case "DL'": minx = move_DL_prime(minx)
+            case "DL2": minx = move_DL2(minx)
+            case "DR": minx = move_DR(minx)
+            case "DR'": minx = move_DR_prime(minx)
+            case "DR2": minx = move_DR2(minx)
+            case "BL": minx = move_BL(minx)
+            case "BL'": minx = move_BL_prime(minx)
+            case "BL2": minx = move_BL2(minx)
+            case "BR": minx = move_BR(minx)
+            case "BR'": minx = move_BR_prime(minx)
+            case "BR2": minx = move_BR2(minx)
+            case "AL": minx = move_AL(minx)
+            case "AL'": minx = move_AL_prime(minx)
+            case "AL2": minx = move_AL2(minx)
+            case "AR": minx = move_AR(minx)
+            case "AR'": minx = move_AR_prime(minx)
+            case "AR2": minx = move_AR2(minx)
+        return minx
     
     def multiMove(self, scr, minx, md, s, delay=200):
         lstMove = s.split(" ")

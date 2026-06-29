70a0cbe17d45fd8de30b2eea043c62e7b261867e
LucaGoubelle <luca.goubelle97@gmail.com>
2026-06-29 18:26:24 +0200

    Implement WB edge solve (remain cases)

--- megaminx_solver/handlers/wb_edge_handler.py
+++ megaminx_solver/handlers/wb_edge_handler.py
@@ -1,5 +1,6 @@
 """ WHITE BLUE edge handler """
 from megaminx_solver.handlers.handler import Handler
+from megaminx_solver.helpers.edge_finder import EdgeFinder
 
 
 class WBEdgeHandler(Handler):
@@ -7,9 +8,19 @@ class WBEdgeHandler(Handler):
     
     def __init__(self, mover):
         super().__init__(mover)
-        self.cases = {}
+        self.edge_finder = EdgeFinder()
+        # TODO: add cases
+        self.cases = {
+            "up_front::white_blue": "",
+            "up_left::white_blue": "y'",
+            "up_right::white_blue": "y",
+            "up_backLeft::white_blue": "y2'",
+            "up_backRight::white_blue": "y2",
+        }
         
     def handle_wb_edge(self, minx):
-        # TODO: implement this
-        # TODO: + add edge finder
+        result_finder = self.edge_finder.find(minx, ["white","blue"])
+        results: str = f"{result_finder['orients']}::{result_finder['colors']}"
+        sequence = self.cases[results] if results in self.cases else ""
+        minx = self.mover.multiMove(minx, sequence)
         return minx
